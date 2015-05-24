b814cdb982a0b93affb1d4a7a0fe3995f5f01c35
Jonathan Minton <nate.minton@gmail.com>
2015-05-24 20:54:27 +0100

    various graphs - start with latticeplot aesthetics

--- script.R
+++ script.R
@@ -25,6 +25,7 @@ require(rgl)
 require(ggplot2)
 require(lattice)
 require(r2stl)
+require(scales)
 
 
 # Data --------------------------------------------------------------------
@@ -59,11 +60,12 @@ dta_england %>%
   facet_wrap(~ year, nrow=4) + 
   geom_hline(aes(yintercept=0), linetype="dashed") +
   theme_minimal() +
+  scale_colour_manual(name="Sex", values=c("red", "blue")) + 
   labs(title="Internal net migration", y="Count", x="Age")
 
-ggsave("figures/internal_net.png", 
+ggsave("figures/internal_net.tiff", 
        dpi=300,
-       width=20, height=20, 
+       width=20, height=25, 
        units="cm"
        )
 
@@ -72,11 +74,16 @@ dta_england %>%
   geom_bar(aes(x=age, y=internal_out, fill=sex, colour=sex, group=sex), stat="identity") + 
   facet_wrap(~ year, nrow=4) + 
   theme_minimal() +
-  labs(title="Internal out-migration", y="Count", x="Age")
+  scale_fill_manual(name="Sex", values=c("red4", "steelblue4")) +
+  scale_colour_manual(name="Sex", values=c("red4", "steelblue4")) +
+  scale_y_continuous(labels=comma) +
+  labs(title="Internal out-migration", y="Count", x="Age") +
+  annotate("rect", xmin=0, xmax=18, ymin=0, ymax=150000, alpha=0.2) +
+  annotate("rect", xmin=60, xmax=91, ymin=0, ymax=150000, alpha=0.2)
 
-ggsave("figures/internal_out.png", 
+ggsave("figures/internal_out.tiff", 
        dpi=300,
-       width=20, height=20, 
+       width=20, height=25, 
        units="cm"
       )
 
@@ -85,11 +92,16 @@ dta_england %>%
   geom_bar(aes(x=age, y=internal_in, fill=sex, colour=sex, group=sex), stat="identity") + 
   facet_wrap(~ year, nrow=4) + 
   theme_minimal() +
-  labs(title="Internal in-migration", y="Count", x="Age")
+  scale_fill_manual(name="Sex", values=c("red4", "steelblue4")) +
+  scale_colour_manual(name="Sex", values=c("red4", "steelblue4")) +
+  scale_y_continuous(labels=comma) +
+  labs(title="Internal in-migration", y="Count", x="Age") +
+  annotate("rect", xmin=0, xmax=18, ymin=0, ymax=150000, alpha=0.2) +
+  annotate("rect", xmin=60, xmax=91, ymin=0, ymax=150000, alpha=0.2)
 
-ggsave("figures/internal_in.png", 
+ggsave("figures/internal_in.tiff", 
        dpi=300,
-       width=20, height=20, 
+       width=20, height=25, 
        units="cm"
 )
 
@@ -99,34 +111,57 @@ dta_england %>%
   geom_line(aes(x=age, y=international_net, colour=sex, group=sex)) + 
   facet_wrap(~ year, nrow=4) + 
   geom_hline(aes(yintercept=0), linetype="dashed") +
-  theme_minimal() +
+  theme_minimal() + 
+  scale_y_continuous(labels=comma) +
+  scale_colour_manual(name="Sex", values=c("red", "blue")) +
   labs(title="International net migration", y="Count", x="Age")
+
+ggsave("figures/international_net.tiff", 
+       dpi=300,
+       width=20, height=25, 
+       units="cm"
+)
+
 
 dta_england %>% 
   ggplot(data=.) + 
   geom_bar(aes(x=age, y=international_in, fill=sex, colour=sex, group=sex), stat="identity") + 
   facet_wrap(~ year, nrow=4) + 
-  geom_hline(aes(yintercept=0), linetype="dashed") +
   theme_minimal() +
-  labs(title="International in-migration", y="Count", x="Age")
+  scale_y_continuous(labels=comma) + 
+  scale_fill_manual(name="Sex", values=c("red4", "steelblue4")) +
+  scale_colour_manual(name="Sex", values=c("red4", "steelblue4")) +
+  labs(title="International in-migration", y="Count", x="Age")  + 
+  annotate("rect", xmin=0, xmax=18, ymin=0, ymax=40000, alpha=0.2) +
+  annotate("rect", xmin=60, xmax=91, ymin=0, ymax=40000, alpha=0.2)
+
+
+ggsave("figures/international_in.tiff", 
+       dpi=300,
+       width=20, height=25, 
+       units="cm"
+)
+
 
 dta_england %>% 
   ggplot(data=.) + 
   geom_bar(aes(x=age, y=international_out, fill=sex, colour=sex, group=sex), stat="identity")  + 
   facet_wrap(~ year, nrow=4) + 
-  geom_hline(aes(yintercept=0), linetype="dashed") +
   theme_minimal() +
-  labs(title="International out-migration", y="Count", x="Age")
+  scale_y_continuous(labels=comma) + 
+  scale_fill_manual(name="Sex", values=c("red4", "steelblue4")) +
+  scale_colour_manual(name="Sex", values=c("red4", "steelblue4")) +
+  labs(title="International out-migration", y="Count", x="Age") + 
+  annotate("rect", xmin=0, xmax=18, ymin=0, ymax=20000, alpha=0.2) +
+  annotate("rect", xmin=60, xmax=91, ymin=0, ymax=20000, alpha=0.2)
+
+ggsave("figures/international_out.tiff", 
+       dpi=300,
+       width=20, height=25, 
+       units="cm"
+)
 
 
-dta_england %>% 
-  ggplot(data=.) + 
-  geom_line(aes(x=age, y=international_in, colour=sex, group=sex), size=1.1) + 
-  geom_line(aes(x=age, y=internal_in, colour=sex, group=sex)) +
-  facet_wrap(~ year, nrow=4) + 
-  geom_hline(aes(yintercept=0), linetype="dashed") +
-  theme_minimal() +
-  labs(title="International (thick) and internal (thin)\n in-migration", y="Count", x="Age")
 
 dta_england %>%
   mutate(mig_prop = international_in / population) %>%
@@ -134,11 +169,18 @@ dta_england %>%
   geom_line(aes(x=age, y=mig_prop, colour=sex, group=sex)) +
   facet_wrap(~ year, nrow=4) + 
   theme_minimal() +
+  scale_colour_manual(name="Sex", values=c("red", "blue")) +
   labs(
     title="Proportion of population who are \ninternational migrants", 
     y="Proportion", x="Age"
-    ) + 
-  geom_vline(aes(xintercept=c(18)))
+    ) 
+
+ggsave("figures/international_immigrant_prop.tiff", 
+       dpi=300,
+       width=20, height=25, 
+       units="cm"
+)
+
 
 dta_england %>%
   mutate(mig_prop = international_out / population) %>%
@@ -146,32 +188,55 @@ dta_england %>%
   geom_line(aes(x=age, y=mig_prop, colour=sex, group=sex)) + 
   facet_wrap(~ year, nrow=4) + 
   theme_minimal() +
+  scale_colour_manual(name="Sex", values=c("red", "blue")) +
   labs(
     title="Proportion of population who \nemmigrate internationally", 
     y="Proportion", x="Age"
-    ) + 
-  geom_vline(aes(xintercept=c(18, 60, 65)))
+    ) 
+ggsave("figures/international_outmigrant_prop.tiff", 
+       dpi=300,
+       width=20, height=25, 
+       units="cm"
+)
+
 
 dta_england %>% 
   ggplot(data=.) + 
   geom_bar(aes(x=age, y=population, fill=sex, colour=sex, group=sex), stat="identity")  + 
   facet_wrap(~ year, nrow=4) + 
-  geom_hline(aes(yintercept=0), linetype="dashed") +
   theme_minimal() +
-  labs(title="Population", y="Count", x="Age")
+  scale_y_continuous(labels=comma) + 
+  scale_fill_manual(name="Sex", values=c("red4", "steelblue4")) +
+  scale_colour_manual(name="Sex", values=c("red4", "steelblue4")) +
+  labs(title="Population", y="Count", x="Age") +
+  annotate("rect", xmin=0, xmax=18, ymin=0, ymax=800000, alpha=0.2) +
+  annotate("rect", xmin=60, xmax=91, ymin=0, ymax=800000, alpha=0.2) 
+  
+ggsave("figures/english_population.tiff", 
+       dpi=300,
+       width=20, height=25, 
+       units="cm"
+)
+
 
 
 # contour plots -----------------------------------------------------------
 
 
 dta_england %>%
+  mutate(population = population / 1000) %>%
+  filter(age <90) %>%
   contourplot(
     population ~ age + year | sex,
     data=.,
     region=T,
     layout=c(1,2), cuts=25,
-    col.regions=colorRampPalette(brewer.pal(6, "Spectral"))(200)
-  )
+    col.regions=colorRampPalette(brewer.pal(6, "Reds"))(200),
+    scales=list(alternating=3),
+    col="blue",
+    strip=strip.custom(bg="lightgrey"),
+    par.strip.text=list(fontface="bold")  
+    )
 
 dta_england %>%
   contourplot(
