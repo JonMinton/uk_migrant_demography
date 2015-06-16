4a86aeefd92678df41089d545332c03d464ea656
Jonathan Minton <nate.minton@gmail.com>
2015-06-16 10:05:39 +0100

    create version of fig with England & Wales only, but Yorkshire/Humber split over two lines.

--- scripts/inflows_and_outflows_in_context_for_uk.R
+++ scripts/inflows_and_outflows_in_context_for_uk.R
@@ -53,3 +53,81 @@ ggsave("figures/regions/uk_inflow_outflow_context.png",
        width=40, height=40, 
        units="cm"
 )
+
+
+# England/Wales migration in context
+
+# prereqs -----------------------------------------------------------------
+
+require(plyr)
+require(stringr)
+require(tidyr)
+require(dplyr)
+
+require(openxlsx) 
+# for scotland population data
+
+require(RColorBrewer)
+require(rgl)
+require(ggplot2)
+require(lattice)
+require(r2stl)
+require(scales)
+
+
+rm(list=ls())
+
+
+dta_uk <- read.csv("data/tidied/UK_population_migration_inc_Scotland.csv") %>% tbl_df
+
+dta_uk <- dta_uk %>% filter(ons_region_name != "Scotland")
+
+levels(
+  dta_uk$ons_region_name
+)[levels(dta_uk$ons_region_name)=="Yorkshire and The Humber"] <- "Yorkshire\n/Humber"
+
+dta_uk$ons_region_name <- factor(dta_uk$ons_region_name, 
+                                 levels =  c(
+                                   "London", "South East", "South West",
+                                   "East of England", "East Midlands", "West Midlands",
+                                   "North East", "North West", "Yorkshire\n/Humber",
+                                   "Wales"
+                                 )
+)
+
+dta_uk  %>% 
+  group_by(age, year, ons_region_name)  %>% 
+  ggplot(.) + 
+  geom_ribbon(
+    aes(x=age, ymin=0, ymax=population),
+    fill="lightgrey"
+  ) + 
+  geom_ribbon(
+    aes(x=age, ymax=0, ymin=-internal_out), 
+    fill="lightblue"
+  ) + 
+  geom_ribbon(
+    aes(x=age, ymax=-internal_out, ymin=-(internal_out + international_out)), 
+    fill="darkblue"
+  ) + 
+  geom_ribbon(
+    aes(x=age, ymin=0, ymax=internal_in), 
+    fill="red"
+  ) + 
+  geom_ribbon(
+    aes(x=age, ymin=internal_in, ymax=(internal_in + international_in)), 
+    fill="darkred") + 
+  facet_grid(ons_region_name ~ year) +
+  theme_minimal() +
+  scale_y_continuous(labels=comma) +
+  labs(title="Inflows and outflows in context", y="Count", x="Age in single years") +
+  annotate("rect", xmin=0, xmax=18, ymin=-20000, ymax=150000, alpha=0.2) +
+  annotate("rect", xmin=60, xmax=91, ymin=-20000, ymax=150000, alpha=0.2) 
+
+ggsave("figures/regions/engwales_inflow_outflow_context.png", 
+       dpi=600,
+       width=40, height=40, 
+       units="cm"
+)
+
+
